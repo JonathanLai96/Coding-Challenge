3c14b2413a241cf0d80c3a14af72a88a06a18240
JonLai96 <j9lai@ucsd.edu>
2017-11-17 15:38:40 -0800

    Just put all the stuff I have up onto github

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CODING_CHALLENGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!!!!!!!!!!!!!!");
+            int[] scores = new int[] {100,200,300};
+            foreach(int i in scores)
+            {
+                Console.WriteLine(i);
+            }
+            dictionaryHelper test = new dictionaryHelper("what");
+            Console.WriteLine(test.matched); 
+        }
+    }
+
+
+}
---
+++ dictionaryHelper.cs
@@ -0,0 +1,13 @@
+using System;   
+        class dictionaryHelper
+    {
+        public String matched;
+        public dictionaryHelper(String s)
+        {
+            this.matched = s;    
+        }
+        public String returnLongest(String[] longest)
+        {
+            return null;//needs to be changed to the string that is to be returned
+        }
+    }
